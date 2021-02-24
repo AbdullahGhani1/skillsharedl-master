fc7b3be3ce56e1f554203d98a3d0a3679a183602
Abdullah Ghani <engr.abdullahghani@gmail.com>
2021-02-24 15:09:57 +0500

    first

---
+++ dl.py
@@ -0,0 +1,17 @@
+import sys
+import os
+from skillshare import Skillshare
+from magic import cookie
+
+# or by class ID:
+# dl.download_course_by_class_id(189505397)
+
+
+def main():
+    dl = Skillshare(cookie)
+    course_url = sys.argv[1]
+    dl.download_course_by_url(course_url)
+
+
+if __name__ == "__main__":
+    main()
